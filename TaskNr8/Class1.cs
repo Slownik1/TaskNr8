fc8aa951a63fb61a7a8801ccd6f41f574496e141
Slownik1 <76266435+Slownik1@users.noreply.github.com>
2021-04-12 13:15:31 +0200

    Add files via upload

---
+++ TaskNr8/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskNr8
+{
+    public enum windSpeed
+    {
+
+        west = 'W',
+        south = 'S',
+        east = 'E',
+        north = 'N',
+
+    }
+    public enum cloudCover
+    {
+
+        duże = 'D',
+        srednie = 'S',
+        male = 'M',
+        brak = 'B',
+
+    }
+
+    struct WeatherStation
+    {
+        
+
+        public double temp;
+        public string date;
+        public double humidity;
+        public windSpeed wind;
+        public cloudCover cloud;
+
+        public WeatherStation(double temp, string date, double humidity, windSpeed wind, cloudCover cloud)
+        {
+            this.cloud = cloud;
+            this.wind = wind;
+            this.temp = temp;
+            this.date = date;
+            this.humidity = humidity;
+        }
+
+        public override string ToString()
+        {
+            return $"Pomiar: {date}, Temperatura: {temp}, Kierunek wiatru: {wind}, Zachmurzenie {cloud}, Wilgotnosc {humidity}";
+        }
+
+
+    }
+}
---
+++ TaskNr8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TaskNr8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WeatherStation S1;
+            S1.temp = 11;
+            S1.wind = windSpeed.north;
+            S1.date = "11-09-2021";
+            S1.humidity = 2;
+            S1.cloud = cloudCover.brak;
+
+            Console.WriteLine(S1.ToString());
+
+        }
+    }
+}
